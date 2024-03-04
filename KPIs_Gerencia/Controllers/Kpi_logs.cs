34af25be26c5aaf668e2e7eeb390bb7847ccc40a
Stephanie Rubi Rubio Mapula <steephru@gmail.com>
2024-03-04 16:48:12 -0700

    Formulario funcionando

--- KPIs_Gerencia/Controllers/Kpi_logs.cs
+++ KPIs_Gerencia/Controllers/Kpi_logs.cs
@@ -2,6 +2,20 @@
 
 namespace KPIs_Gerencia.Controllers
 {
-   
+    public class Kpi_logs : Controller
+    {
+        public IActionResult Crear() 
+        { 
+            return View();
+        }
+        
+
+
+        [HttpPost]
+        public IActionResult Crear(Kpi_logs kpi_Logs) 
+        {
+        return View();
+        }
+    }
 }
 
